e259ecb5324e3a954d630010271b24ae297c6f2d
jgarcia4j <joe.garcia@utexas.edu>
2015-04-01 05:11:02 -0500

    Adds document title to MessageContent using Jsoup.

--- src/main/java/com/jgarcia/messageparser/MessageContentParser.java
+++ src/main/java/com/jgarcia/messageparser/MessageContentParser.java
@@ -1,20 +1,38 @@
 package com.jgarcia.messageparser;
 
+import com.jgarcia.LinkParser;
+import com.jgarcia.messageparser.model.Link;
 import com.jgarcia.messageparser.model.MessageContent;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Parses a message for specialized content (mentions, emoticons, links).
  */
 public class MessageContentParser {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MessageContentParser.class);
+
     public MessageContent parse(final String message) {
         final MentionParser mentionParser = new MentionParser();
         final List<String> mentions = mentionParser.parseMentions(message);
-        return new MessageContent(mentions, null, null);
+        final List<String> emoticons = new EmoticonParser().parse(message);
+        final List<String> urls = new LinkParser().parse(message);
+        final List<Link> links = new ArrayList<>(urls.size());
+        for (final String url : urls) {
+            try {
+                final Document document = Jsoup.connect(url).get();
+                links.add(new Link(url, document.title()));
+            } catch (IOException e) {
+                LOG.warn("Unable to get document title: url={}", url);
+            }
+        }
+        return new MessageContent(mentions, emoticons, links);
     }
 }
