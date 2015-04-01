7114f603172f6ef5ae824aea7943e52efdc27646
jgarcia4j <joe.garcia@utexas.edu>
2015-04-01 03:08:23 -0500

    Adds target directory to .gitignore

---
+++ src/main/java/com/jgarcia/messageparser/MentionParser.java
@@ -0,0 +1,7 @@
+package com.jgarcia.messageparser;
+
+/**
+ * Created by lohrb_000 on 4/1/2015.
+ */
+public class MentionParser {
+}
---
+++ src/test/java/com/jgarcia/messageparser/MessageContentParserTest.java
@@ -0,0 +1,10 @@
+package com.jgarcia.messageparser;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lohrb_000 on 4/1/2015.
+ */
+public class MessageContentParserTest {
+
+}
